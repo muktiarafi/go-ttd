a6f83c49ca275b55447d3a0b3b9cbdfd00af6659
muktiarafi <muktiarafi@gmail.com>
2020-11-13 21:16:48 +0700

    refactor hello test case

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Bambank")
-	want := "Hello, Bambank"
+	t.Run("say hello to someone", func(t *testing.T) {
+		got := Hello("Bambank")
+		want := "Hello, Bambank"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say `Hello, World' when passing empty string argument", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
