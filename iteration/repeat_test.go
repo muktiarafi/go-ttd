2dab91bbba05c673657de7e57f8ade810bdcc2f7
muktiarafi <muktiarafi@gmail.com>
2020-11-13 22:23:07 +0700

    add benchmark for repeat function

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -2,6 +2,12 @@ package iteration
 
 import "testing"
 
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
 	expected := "aaaaa"
