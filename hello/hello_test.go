f9768f99c06789eb132fed59d74686191a70d6b5
muktiarafi <muktiarafi@gmail.com>
2020-11-13 21:18:57 +0700

    add test helper function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,21 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("say hello to someone", func(t *testing.T) {
-		got := Hello("Bambank")
-		want := "Hello, Bambank"
-
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("say hello to someone", func(t *testing.T) {
+		got := Hello("Bambank")
+		want := "Hello, Bambank"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say `Hello, World' when passing empty string argument", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
