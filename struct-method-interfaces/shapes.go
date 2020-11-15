8da595c9d08532f6a2c4872baec1cca6131fe301
muktiarafi <muktiarafi@gmail.com>
2020-11-15 19:27:35 +0700

    add rectangle struct

--- struct-method-interfaces/shapes.go
+++ struct-method-interfaces/shapes.go
@@ -1,9 +1,14 @@
 package main
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width  float64
+	Heigth float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Heigth)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Heigth
 }
