18e2b48b52d25bbb76bb6da9d8283fe5281fbae0
muktiarafi <muktiarafi@gmail.com>
2020-11-17 10:14:18 +0700

    add method and configure test suite

--- struct-method-interfaces/shapes.go
+++ struct-method-interfaces/shapes.go
@@ -1,8 +1,22 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Heigth float64
+}
+
+func (r Rectangle) Area() float64 {
+	return 0
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
