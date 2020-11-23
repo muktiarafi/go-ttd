347ab4bc18c9e16cdd3a1bcadcecbd140bdefeb8
muktiarafi <muktiarafi@gmail.com>
2020-11-23 14:01:23 +0700

    reset countdown and add configurable sleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -17,15 +17,21 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
-	}
-
-	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
 
