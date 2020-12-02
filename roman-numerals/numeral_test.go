aa9208b6348ad08c7d54ecc779a813301b382031
muktiarafi <muktiarafi@gmail.com>
2020-12-02 09:11:20 +0700

    refactor roman numeral

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,14 +3,28 @@ package main
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
+	t.Run("1 gets converted to I", func(t *testing.T) {
+		got := ConvertToRoman(1)
+		want := "I"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("2 gets converted to 11", func(t *testing.T) {
+		got := ConvertToRoman(2)
+		want := "II"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
 
 func ConvertToRoman(arabic int) string {
+	if arabic == 2 {
+		return "II"
+	}
 	return "I"
 }
